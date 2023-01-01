a9588a8d5177663b4589c6161ccede87b2ac6ffe
Jemison Silva <78374534+DevSilva-BR@users.noreply.github.com>
2023-01-01 11:32:09 -0300

    Update Firebase.js

--- src/service/Firebase.js
+++ src/service/Firebase.js
@@ -3,13 +3,7 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 let firebaseConfig = {
-    apiKey: "AIzaSyD5PgQxWi5LPBLP9ErwjXG9U9gM_3BiJZQ",
-    authDomain: "kxafreelancer-e125a.firebaseapp.com",
-    projectId: "kxafreelancer-e125a",
-    storageBucket: "kxafreelancer-e125a.appspot.com",
-    messagingSenderId: "925756198215",
-    appId: "1:925756198215:web:e27568a61e11791f499062",
-    measurementId: "G-R0Z3946TGE",
+
 };
 
 if (!firebase.apps.length) {
